03a8b8c87904bca97b7ecf0a0ce2d297b6ab05c8
wolfie <yavor.hristozov@gmail.com>
2023-06-13 17:59:48 +0300

    01. Azure Resource Group
    Create Resource Group in Azure with Terraform

---
+++ 09. Exercise IaaC and Monitoring/azure-rg.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.59.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-resourcegroup-yavor"
+  location = "West Europe"
+}
